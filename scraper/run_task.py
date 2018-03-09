4a6f05581937b4df28741e8d9c90efb81092c1d6
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-03-08 19:26:07 -0600

    Add files via upload

--- scraper/run_task.py
+++ scraper/run_task.py
@@ -1,17 +1,20 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-def job_function:
-    '''
-    define the task you wanna run.
+def job_function():
+	'''
+	define the task you wanna run.
 
-    e.g. print("hello world!")
-    '''
-    
-    return
+	e.g. print("hello world!")
+	print 'Hello world!' during March 7 to 28 every day from nine o' clock
+	to 12 o' clock every 15 minutes.
+	'''
+	print('Hello world!')
+
+	return
 
 sched = BlockingScheduler()
 # set the parameters as you want
-sched.add_job(job_function, 'cron', month='2', day='7-28', hour='9-12', \
+sched.add_job(job_function, 'cron', month='3', day='7-28', hour='9-12', \
               minute='0-59/15')
 
 sched.start()
