2c2e7cce8cde95a9f3325c6fb60aa5673a17253c
Hyun Ki Kim <31553367+coreanokim@users.noreply.github.com>
2018-03-12 21:40:24 -0500

    Update PCR_PLS.py

--- code/method/PCR_PLS.py
+++ code/method/PCR_PLS.py
@@ -1,8 +1,7 @@
-#run this file and can get json file for prediction for PCR, PLS
-#model inside a json file
 #documentation: https://github.com/JWarmenhoven/ISLR-python/blob/master/Notebooks/Chapter%206.ipynb
-import pandas as pd
 import numpy as np
+import pandas as pd
+import json
 import matplotlib.pyplot as plt
 
 from sklearn.decomposition import PCA
@@ -10,104 +9,9 @@ from sklearn.model_selection import KFold, cross_val_score
 from sklearn.metrics import mean_squared_error, classification_report,\
                             confusion_matrix
 from sklearn.cross_decomposition import PLSRegression
-
 from sklearn.linear_model import LogisticRegression
 from pylab import rcParams
 from sklearn.preprocessing import scale 
-import json
-
-
-with open("df_1hour_Feb.json", 'r', encoding='UTF-8') as f:
-    F_data = json.load(f)
-
-with open("df_1hour_Mar_07.json", 'r', encoding='UTF-8') as f:
-    M_data = json.load(f)
-
-COLUMNS = ['name', 'code', 'time', 'price', 'time_1', 'price_1', \
-           'price_dif_1', 'sell_1', 'buy_1', 'volume_1', 'variation_1', \
-           'post_num_1', 'unique_id_1', 'click_1', 'like_1', 'dislike_1', \
-           'time_2', 'price_2', 'price_dif_2', 'sell_2', 'buy_2', \
-           'volume_2', 'variation_2', 'post_num_2', 'unique_id_2', \
-           'click_2', 'like_2', 'dislike_2', 'time_3', 'price_3', \
-           'price_dif_3', 'sell_3', 'buy_3', 'volume_3', 'variation_3', \
-           'post_num_3', 'unique_id_3', 'click_3', 'like_3', 'dislike_3', \
-           'mkt_cap', 'kospi', 'kosdaq', 'trash', 'yesterday_closing_price', \
-           'is_maximum', 'is_minimum', 'price_volatility', 'price_trend', \
-           'average_price_volatility', 'sell_minus_buy_1', \
-           'sell_minus_buy_2', 'sell_minus_buy_3', 'is_price_gap_stable', \
-           'price_gap_volatility', 'is_like_higher', 'volume_trend', \
-           'post_num_trend', 'unique_id_trend', 'click_trend', \
-           'price_increase', 'did_price_increase', 'did_price_033', \
-           'did_price_100', 'did_price_150', 'kospi_ind', 'kosdaq_ind', \
-           'time_slot', 'ko_inter', 'early_mor', 'morning', 'lunch', \
-           'afternoon', 'late', 'mkt_change', 'alpha', 'per_now', \
-           'kospi_1', 'kospi_2', 'kospi_3', 'kospi_answer', 'kosdaq_1', \
-           'kosdaq_2', 'kosdaq_3', 'kosdaq_answer', 'kospi_trend', \
-           'kosdaq_trend', 'kospi_increase', 'kosdaq_increase', \
-           'market_increase', 'did_opening_price_increase', 'price_1_sq', \
-           'price_dif_1_sq', 'sell_1_sq', 'buy_1_sq', 'volume_1_sq', \
-           'variation_1_sq', 'post_num_1_sq', 'unique_id_1_sq', \
-           'click_1_sq', 'like_1_sq', 'dislike_1_sq', 'price_2_sq', \
-           'price_dif_2_sq', 'sell_2_sq', 'buy_2_sq', 'volume_2_sq', \
-           'variation_2_sq', 'post_num_2_sq', 'unique_id_2_sq', \
-           'click_2_sq', 'like_2_sq', 'dislike_2_sq', 'price_3_sq', \
-           'price_dif_3_sq', 'sell_3_sq', 'buy_3_sq', 'volume_3_sq', \
-           'variation_3_sq', 'post_num_3_sq', 'unique_id_3_sq', \
-           'click_3_sq', 'like_3_sq', 'dislike_3_sq', 'mkt_cap_sq', \
-           'yesterday_closing_price_sq', 'price_volatility_sq', \
-           'price_trend_sq', 'average_price_volatility_sq', \
-           'sell_minus_buy_1_sq', 'sell_minus_buy_2_sq', \
-           'sell_minus_buy_3_sq', 'price_gap_volatility_sq', \
-           'volume_trend_sq', 'post_num_trend_sq', 'unique_id_trend_sq', \
-           'click_trend_sq', 'kospi_ind_sq', 'kosdaq_ind_sq', \
-           'time_slot_sq', 'ko_inter_sq', 'mkt_change_sq', 'alpha_sq', \
-           'per_now_sq', 'kospi_1_sq', 'kospi_2_sq', 'kospi_3_sq', \
-           'kosdaq_1_sq', 'kosdaq_2_sq', 'kosdaq_3_sq', 'kospi_trend_sq', \
-           'kosdaq_trend_sq']
-
-TO_DEL = ['name', 'code', 'time', 'price', 'time_1', 'time_2', 'time_3', \
-          'price_increase', 'did_price_increase', 'did_price_033', \
-          'did_price_100', 'did_price_150', 'kospi_answer', \
-          'kosdaq_answer', 'kospi_increase', 'kosdaq_increase', \
-          'market_increase']
-
-X_COL = COLUMNS
-for col in TO_DEL:
-  X_COL = list(X_COL)
-  X_COL.remove(col)
-
-  
-DF_F = pd.DataFrame(F_data, columns = COLUMNS)
-DF_M = pd.DataFrame(M_data, columns = COLUMNS)
-DF = pd.concat([DF_F, DF_M])
-DF = DF.dropna(axis=0, how='any')
-
-#define filter for trainig set, validation test and testing set
-TIME_FILTER_TRAIN = (DF['time'].str.startswith("2018-02-21")) | \
-                    (DF['time'].str.startswith("2018-02-20")) | \
-                    (DF['time'].str.startswith("2018-02-14")) | \
-                    (DF['time'].str.startswith("2018-02-22")) | \
-                    (DF['time'].str.startswith("2018-02-23")) | \
-                    (DF['time'].str.startswith("2018-02-26")) 
-
-TIME_FILTER_TEST = (DF['time'].str.startswith("2018-02-28")) | \
-                   (DF['time'].str.startswith("2018-03-02")) | \
-                   (DF['time'].str.startswith("2018-03-05")) | \
-                   (DF['time'].str.startswith("2018-02-27")) | \
-                   (DF['time'].str.startswith("2018-03-06")) | \
-                   (DF['time'].str.startswith("2018-03-07"))
-
-TRAIN_DF = DF[TIME_FILTER_TRAIN].reset_index(drop = True)
-TEST_DF = DF[TIME_FILTER_TEST].reset_index(drop = True)
-
-#split X and y dataframe, hardcoding y variable is 'did_price_033'
-X = DF[X_COL]
-Y = DF['did_price_033']
-X_TRAIN = TRAIN_DF[X_COL]
-Y_TRAIN = TRAIN_DF['did_price_033']
-X_TEST = TEST_DF[X_COL]
-Y_TEST = TEST_DF['did_price_033']
-TEST_IN = TEST_DF['price_increase']
 
 #PCR
 class PCR_mod:
@@ -230,7 +134,6 @@ class PCR_mod:
         return        
 
 
-
     def get_confusion_matrix(self, y_test, y_pred):
         '''
         Given the testing set and prediction for y (categorical) and return
@@ -287,7 +190,6 @@ PCR = PCR_mod(2, X_TRAIN, Y_TRAIN, X_TEST, Y_TEST, TEST_IN)
 y_pred_pcr = PCR.y_pred
 PCR_mod = pd.DataFrame(y_pred_pcr, columns = ['PCR'])
 PCR_mod.to_json('PCR_mod.json', orient='values')
-
 
 
 
@@ -436,9 +338,3 @@ y_pred_pls = PLS.y_pred
 PLS_mod = pd.DataFrame(y_pred_pls, columns = ['PLS'])
 PLS_mod['PLS'] = (PLS_mod['PLS'] >= 0.5).astype(int)
 PLS_mod.to_json('PLS_mod.json', orient='values')
-
-
-
-
-
-
