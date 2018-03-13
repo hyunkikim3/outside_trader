c5e1065376cdf601d17ab95a19f5d1675115fbb3
Hyun Ki Kim <31553367+coreanokim@users.noreply.github.com>
2018-03-13 12:12:10 -0500

    Update Tree.py

--- code/method/Tree.py
+++ code/method/Tree.py
@@ -2,17 +2,42 @@ import numpy as np
 import pandas as pd
 import json
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import export_graphviz
+import graphviz
 
 class Tree_model:
     
-    def __init__(self, max_depth, min_samples, x_test):
+    def __init__(self, max_depth, min_samples, x_train, y_train, \
+                 x_test, y_test):
         self.max_depth = max_depth
         self.min_samples = min_samples
+        self.x_train = x_train
+        self.y_train = y_train
         self.x_test = x_test
-        self.y_pred = self.best_prediction(x_test)
+        self.y_test = y_test
+        self.y_pred = self.get_best_prediction(x_test)
     
-    def best_prediction(df):
+    def get_prediction(self, x_train, y_train, x_test):
+        df_tree = DecisionTreeClassifier(max_depth=self.max_depth, \
+                                         min_samples_leaf=self.min_samples)
+        df_tree.fit(X_train, y_train)
+        
+        return df_tree.predict(X_test)
+    
+    def get_mean_squared_error(self, y_test, y_pred):
+        return mean_squared_error(y_test, y_pred)
+        
+    def get_best_prediction(self, df):
         return ((df["price_volatility"] > 0.5) & \
                 (df["average_price_volatility"] <= 0) &\
                 (df["alpha"] <= -1) & \
                 (df["variation_2_sq"] <= 200)).astype(int)
+    
+    def visualize_tree(self, filename):
+        df_tree_viz = export_graphviz(df_tree, out_file=None, \
+                                      feature_names=X, \
+                                      rounded=True, filled=True)
+        graph = graphviz.Source(df_tree_viz)
+        graph.render(filename)
+        return None
